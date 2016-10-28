6442114f86fd70cda133425da0718b549dd00d32
Tyrone Davison <t.p.davison@tees.ac.uk>
2016-10-28 02:36:25 +0100

    Add actionlink to update the customer list from a webservice.

--- MyMinions.App/Controllers/CustomersController.cs
+++ MyMinions.App/Controllers/CustomersController.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,51 @@ namespace MyMinions.App.Controllers
                                    })
                                    .AsEnumerable();
             return View(customers);
+        }
+
+        public async Task<ActionResult> Update()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new System.Uri("http://myminions.azurewebsites.net/myminions/");
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            client.Timeout = TimeSpan.FromMilliseconds(5000);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("api/customer");
+                response.EnsureSuccessStatusCode();
+                var customers =
+                    (await response.Content.ReadAsAsync<IEnumerable<Models.CustomerHttp>>())
+                                   .Select(c => new Model.Customer
+                                   {
+                                       Id = c.Id,
+                                       FirstName = c.FirstName,
+                                       LastName = c.LastName,
+                                       Age = c.Age,
+                                       Reputation = c.Reputation
+                                   })
+                                   .ToList();
+                customers.ForEach(c =>
+                {
+                    var customer = context.Customers.Find(c.Id);
+                    if (customer == null)
+                    {
+                        context.Customers.Add(c);
+                    }
+                    else
+                    {
+                        customer.Age = c.Age;
+                        customer.FirstName = c.FirstName;
+                        customer.LastName = c.LastName;
+                        customer.Reputation = c.Reputation;
+                        context.Entry(customer).State = EntityState.Modified;
+                    }
+                });
+                await context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+            }
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
---
+++ MyMinions.App/Models/CustomerHttp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMinions.App.Models
+{
+    public class CustomerHttp
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Reputation { get; set; }
+    }
+}
