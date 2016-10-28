89ed7f019f90f062e835c0a04879694c19a7ac1e
Tyrone Davison <t.p.davison@tees.ac.uk>
2016-10-28 01:31:33 +0100

    Add Customer controller with index list and view, pulling from EF model.

---
+++ MyMinions.App/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMinions.App.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private Model.Context context = new Model.Context();
+
+        public ActionResult Index()
+        {
+            var customers = context.Customers
+                                   .OrderBy(c => c.LastName)
+                                   .Select(c => new Models.CustomerIndex
+                                   {
+                                       Id = c.Id,
+                                       FirstName = c.FirstName,
+                                       LastName = c.LastName
+                                   })
+                                   .AsEnumerable();
+            return View(customers);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
